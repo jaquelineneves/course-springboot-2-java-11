8b5c93aebee5d4765370e2463422b71eec1befb7
jaqueline <jaque.nevees2@gmail.com>
2019-08-26 11:22:59 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);//p1 dentro da coleção de categorias dele, esta o cat2
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		
 		// td que tiver aqui vai ser executado qdo a aplicação for iniciada
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
