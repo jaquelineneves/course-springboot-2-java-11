ee4bc685bd90db52a597ea2d023afd89d16b7844
Jaqueline <jaque.nevees2@gmail.com>
2019-09-14 10:21:38 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -102,6 +102,15 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	//total do pedido
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
