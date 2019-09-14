b7e9ca9156f65009c88112a2ec3533f62952b962
Jaqueline <jaque.nevees2@gmail.com>
2019-09-14 14:03:42 -0300

    Challenge: category CRUD

--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -3,11 +3,19 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import services.exceptions.DatabaseException;
 
 @Service
 public class CategoryService {
@@ -21,6 +29,38 @@ public class CategoryService {
 	
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	//retorna o usuario salvo
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	//deletar o usuario do bd
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	//atualizar um usuario (passa o id, e um objeto usuario com os dados a serem atualizados)
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj); //atualizar o meu entity com os dados do obj
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	//atualiza os dados do entity com base no que chegou no obj
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());	
 	}
 }
