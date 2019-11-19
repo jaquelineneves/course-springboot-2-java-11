cd1b15fab2fd43f9d03f052f20ac71394c2e76d4
jaquelineneves <jaque.nevees2@gmail.com>
2019-11-19 18:15:12 -0200

    Refresh token

--- src/main/java/com/educandoweb/course/services/AuthService.java
+++ src/main/java/com/educandoweb/course/services/AuthService.java
@@ -71,4 +71,10 @@ public class AuthService {
 			throw new JWTAuthorizationException("Acess denied");
 		}
 	}
+	
+	public TokenDTO refreshToken() {
+		User user= authenticated();
+		return new TokenDTO(user.getEmail(),jwtUtil.generateToken(user.getEmail()));
+
+	}
 }
