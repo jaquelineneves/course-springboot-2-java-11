5db7500df3bf2a1b202bfc685c59ae1212e320f5
jaqueline <jaque.nevees2@gmail.com>
2019-11-20 10:46:24 -0300

    Payment PUT POST endpoints

--- src/main/java/com/educandoweb/course/resources/PaymentResource.java
+++ src/main/java/com/educandoweb/course/resources/PaymentResource.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,4 +40,21 @@ public class PaymentResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping
+	public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO dto){
+		dto= service.insert(dto);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value="/{id}")
+	public ResponseEntity<PaymentDTO> update(@PathVariable Long id,@RequestBody PaymentDTO dto){
+		dto= service.update(id,dto);
+		return ResponseEntity.ok().body(dto);
+
+	}
 }
